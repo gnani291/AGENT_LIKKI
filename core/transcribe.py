241171933dd8ecb935dfae5b410383e0a7062b65
Tadiparthi Gnaneswar <gnanitadiparthi@gmail.com>
2026-08-15 18:56:58 +0530

    Update transcribe.py

--- core/transcribe.py
+++ core/transcribe.py
@@ -2,6 +2,7 @@ from faster_whisper import WhisperModel
 
 _model_cache = {}
 
+
 def _get_model(model_size: str = "base"):
     if model_size not in _model_cache:
         _model_cache[model_size] = WhisperModel(model_size, device="cpu", compute_type="int8")
